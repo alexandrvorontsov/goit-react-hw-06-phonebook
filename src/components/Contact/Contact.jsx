2c74d3092c473ece5313f795c8bb8686f36a14c1
Alexander Vorontsov <108517482+alexandrvorontsov@users.noreply.github.com>
2023-10-21 15:44:36 +0300

    finish

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import ContactForm from '../ContactForm/ContactForm';
 import ContactList from '../ContactList/ContactList';
 import Filter from '../Filter/Filter';
@@ -13,31 +13,15 @@ import {
 } from 'redux/contactListReducer';
 
 export default function Contact() {
-  // const LS_KEY = 'contacts';
-  // const [contacts, setContacts] = useState([]);
-  // const [filter, setFilter] = useState('');
-  // const [isDelete, setIsDelete] = useState(false);
-  // const [isCreate, setIsCreate] = useState(false);
-
   const contacts = useSelector(state => state.contactList.contactsData);
   const contactFilter = useSelector(state => state.contactList.filterData);
   const isDelete = useSelector(state => state.contactList.isDelete);
   const isCreate = useSelector(state => state.contactList.isCreate);
   const dispatch = useDispatch();
 
-  // useEffect(() => {
-  // const localTodo = localStorage.getItem(LS_KEY || []);
-  // if (localTodo) setContacts(JSON.parse(localTodo));
-  // }, []);
-
-  // useEffect(() => {
-  // contacts && localStorage.setItem(LS_KEY, JSON.stringify(contacts));
-  // }, [contacts]);
-
   const handleChange = evt => {
     const { value } = evt.currentTarget;
     dispatch(setFilter(value));
-    // setFilter(value);
   };
 
   const addContact = value => {
@@ -85,79 +69,3 @@ export default function Contact() {
     </Phonebook>
   );
 }
-//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-// import React, { useEffect, useState } from 'react';
-// import ContactForm from '../ContactForm/ContactForm';
-// import ContactList from '../ContactList/ContactList';
-// import Filter from '../Filter/Filter';
-// import { Phonebook, MessageDelete, MessageCreate } from './App.styled';
-
-// export default function App() {
-//   const LS_KEY = 'contacts';
-//   const [contacts, setContacts] = useState([]);
-//   const [filter, setFilter] = useState('');
-//   const [isDelete, setIsDelete] = useState(false);
-//   const [isCreate, setIsCreate] = useState(false);
-
-//   useEffect(() => {
-//     const localTodo = localStorage.getItem(LS_KEY || []);
-//     if (localTodo) setContacts(JSON.parse(localTodo));
-//   }, []);
-
-//   useEffect(() => {
-//     contacts && localStorage.setItem(LS_KEY, JSON.stringify(contacts));
-//   }, [contacts]);
-
-//   const handleChange = evt => {
-//     const { value } = evt.currentTarget;
-//     setFilter(value);
-//   };
-
-//   const addContact = value => {
-//     const { name } = value;
-//     const isContactExist = contacts.find(
-//       contact => contact.name.toLowerCase() === name.toLowerCase()
-//     );
-//     if (isContactExist) {
-//       return alert(`${name} is already in contacts`);
-//     }
-
-//     setContacts(prevTodoList => {
-//       return [...prevTodoList, value];
-//     });
-//     setIsCreate(true);
-//     setTimeout(() => {
-//       setIsCreate(false);
-//     }, 1000);
-//   };
-
-//   const handleFilter = () => {
-//     const normalizedFilter = filter.toLowerCase();
-//     return contacts.filter(prevContacts =>
-//       prevContacts.name.toLowerCase().includes(normalizedFilter)
-//     );
-//   };
-
-//   const handleDelete = elementToRemove => {
-//     setContacts(prevContacts =>
-//       prevContacts.filter(contact => contact.id !== elementToRemove)
-//     );
-//     setIsDelete(true);
-//     setTimeout(() => {
-//       setIsDelete(false);
-//     }, 1000);
-//   };
-
-//   return (
-//     <Phonebook>
-//       <h1>Phonebook</h1>
-//       <ContactForm addContact={addContact} />
-//       <h2>Filter</h2>
-//       <Filter filter={filter} onChange={handleChange} />
-//       <h2>Contacts</h2>
-//       <ContactList getContacts={handleFilter()} deleteContact={handleDelete} />
-//       {isDelete && <MessageDelete>Contact delete successfullly!</MessageDelete>}
-//       {isCreate && <MessageCreate>Contact create successfullly!</MessageCreate>}
-//     </Phonebook>
-//   );
-// }
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -10,8 +10,6 @@ import {
 } from './ContactForm.styled';
 
 export default function ContactForm({ addContact }) {
-  // const [name, setName] = useState('');
-  // const [number, setNumber] = useState('');
   const name = useSelector(state => state.contactForm.nameData);
   const number = useSelector(state => state.contactForm.numberData);
   const dispatch = useDispatch();
@@ -21,11 +19,11 @@ export default function ContactForm({ addContact }) {
     switch (name) {
       case 'name':
         dispatch({ type: 'contactForm/setNameData', payload: value });
-        // setName(value);
+
         break;
       case 'number':
         dispatch({ type: 'contactForm/setNumberData', payload: value });
-        // setNumber(value);
+
         break;
       default:
         return;
@@ -41,8 +39,6 @@ export default function ContactForm({ addContact }) {
   const resetForm = () => {
     dispatch({ type: 'contactForm/setNameData', payload: '' });
     dispatch({ type: 'contactForm/setNumberData', payload: '' });
-    // setName('');
-    // setNumber('');
   };
 
   return (
@@ -80,79 +76,3 @@ export default function ContactForm({ addContact }) {
 ContactForm.propTypes = {
   addContact: PropTypes.func.isRequired,
 };
-
-//++++++++++++++++++++++++++++++++++++
-// import React, { useState } from 'react';
-// import PropTypes from 'prop-types';
-// import { nanoid } from 'nanoid';
-// import {
-//   BodyForm,
-//   LabelForm,
-//   InputForm,
-//   ButtonForm,
-// } from './ContactForm.styled';
-
-// export default function ContactForm({ addContact }) {
-//   const [name, setName] = useState('');
-//   const [number, setNumber] = useState('');
-
-//   const handleChange = evt => {
-//     const { name, value } = evt.target;
-//     switch (name) {
-//       case 'name':
-//         setName(value);
-//         break;
-//       case 'number':
-//         setNumber(value);
-//         break;
-//       default:
-//         return;
-//     }
-//   };
-
-//   const handleSubmit = evt => {
-//     evt.preventDefault();
-//     addContact({ id: nanoid(), name: name, number: number });
-//     resetForm();
-//   };
-
-//   const resetForm = () => {
-//     setName('');
-//     setNumber('');
-//   };
-
-//   return (
-//     <BodyForm onSubmit={handleSubmit}>
-//       <LabelForm>
-//         Name
-//         <InputForm
-//           placeholder="Jacob Mercer"
-//           type="text"
-//           name="name"
-//           required
-//           pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-//           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-//           value={name}
-//           onChange={handleChange}
-//         />
-//       </LabelForm>
-//       <LabelForm>
-//         Number
-//         <InputForm
-//           placeholder="555-55-55"
-//           type="tel"
-//           name="number"
-//           required
-//           pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
-//           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-//           value={number}
-//           onChange={handleChange}
-//         />
-//       </LabelForm>
-//       <ButtonForm type="submit">Add contact</ButtonForm>
-//     </BodyForm>
-//   );
-// }
-// ContactForm.propTypes = {
-//   addContact: PropTypes.func.isRequired,
-// };
